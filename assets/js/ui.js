419623fec7138cd4e203cf181b193550d69c2a53
Sam Potts <me@sampotts.me>
2015-01-02 23:28:46 +1100

    Scrolling

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -3,27 +3,37 @@
 // --------------------------------------------
 
 (function($) {
-    var $body,
+    var $window,
+        $body,
         config = {
             selector: {
-                toggleClass: "[data-toggle='class']"
+                toggleClass:    "[data-toggle='class']",
+                smoothScroll:   ".js-smooth-scroll"
             }
         };
 
     $(function() {
         $body = $(document.body);
+        $window = $(window);
 
         setup();
 
         // Setup UI
         function setup() {
             listeners();
+
+            // When our page loads, check to see if it contains and anchor
+            scrollToAnchor();
         }
 
         // Event bindings
         function listeners() {
+            $window
+                .on("hashchange", scrollToAnchor);
+
             $body
-                .on("click", config.selector.toggleClass, toggleClass);
+                .on("click", config.selector.toggleClass, toggleClass)
+                .on("click", config.selector.smoothScroll, smoothScroll);
         }
 
         function toggleClass() {
@@ -34,6 +44,46 @@
             if(!!$target.length) {
                 $target.toggleClass(className);
             }
+        }
+
+        // Scroll offset on anchors
+        // ---------------------------------
+        function scrollToAnchor() {
+            // Re-use for below
+
+            var hash = window.location.hash,
+                hashFormat = new RegExp(/^[a-z0-9-_]+$/i);
+
+            if(hash.indexOf("#") === 0 && hashFormat.test(hash.replace("#", ""))) {
+                // Calculate offset
+                var $target = $(hash);
+
+                if($target.length) {
+                    $("html, body").animate({
+                        scrollTop: $target.offset().top
+                    }, 300, "swing");
+                }
+            }
+        }
+
+        function smoothScroll(e) {
+            e.preventDefault();
+            var $this   = $(this),
+                hash    = $this.attr("href").split("#")[1];
+
+            // Calculate destination place
+            var dest = 0;
+
+            if($("#" + hash).offset().top > $(document).height()-$window.height()) {
+                dest = $(document).height() - $window.height();
+            }
+            else {
+                dest = $("#" + hash).offset().top - ($this.data("offset") || ($this.data("offset-target") ? $($this.data("offset-target")).outerHeight() : 0));
+            }
+            //go to destination
+            $("html, body").animate({
+                scrollTop: dest
+            }, 300, "swing");
         }
     });
 })(jQuery);
